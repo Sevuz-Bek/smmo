9fc12bca71a3b890fec3cb779ebc699c63b3f169
Eugen <eugen.haidukov@netzlabor-coding.de>
2020-03-27 18:15:26 +0300

    Add page All-html + improve styles of header-menu

--- Dev/Pages/src/js/components/burger-menu.js
+++ Dev/Pages/src/js/components/burger-menu.js
@@ -3,7 +3,7 @@ export default {
     $('.header__burger').on(
       "click",
       () => {
-        $('.header__burger, .header__menu').toggleClass('active');
+        $('.header__burger, .header__menu-wrapper').toggleClass('active');
         $('body').toggleClass('lock');
       }
     );
